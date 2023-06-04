89c0a18093546fe63a24faea4fae0ba7b6cbc8f0
Batool Fatima <batoolfatima2135@gmail.com>
2023-06-04 15:09:02 -0700

    Fix linter error

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import add  from './modules/form-submit.js';
+import add from './modules/form-submit.js';
 import BookList from './modules/book-class.js';
 import navbar from './modules/navigation-links.js';
-
 
 const newBook = new BookList();
 add();
--- modules/form-submit.js
+++ modules/form-submit.js
@@ -1,14 +1,15 @@
 import BookList from './book-class.js';
-export default function add(){
-    document.querySelector('#submit-button').addEventListener('submit', () => logSubmit());
-   function logSubmit(event) {
-      const newBook = new BookList();
-      const bookTitle = document.querySelector('#book-title').value;
-      const bookAuthor = document.querySelector('#book-author').value;
-      newBook.addBooks(bookTitle, bookAuthor);
-      newBook.displayBooks();
-      document.querySelector('#book-title').value = '';
-      document.querySelector('#book-author').value = '';
-      event.preventDefault();
-    }
+
+export default function add() {
+  function logSubmit(event) {
+    const newBook = new BookList();
+    const bookTitle = document.querySelector('#book-title').value;
+    const bookAuthor = document.querySelector('#book-author').value;
+    newBook.addBooks(bookTitle, bookAuthor);
+    newBook.displayBooks();
+    document.querySelector('#book-title').value = '';
+    document.querySelector('#book-author').value = '';
+    event.preventDefault();
+  }
+  document.querySelector('#submit-button').addEventListener('submit', () => logSubmit());
 }
