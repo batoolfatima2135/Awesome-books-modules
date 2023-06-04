b82bc30c85192270cde69c5d59adc3a31da4646e
Batool Fatima <batoolfatima2135@gmail.com>
2023-06-04 15:02:13 -0700

    Solve error of index.js eventlistner

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import logSubmit  from './modules/form-submit.js';
 import BookList from './modules/book-class.js';
 import navbar from './modules/navigation-links.js';
-import logSubmit from './modules/form-submit.js';
+
 
 const newBook = new BookList();
 document.querySelector('#submit-button').addEventListener('submit', () => logSubmit());
