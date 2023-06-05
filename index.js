cd35e50e124462c4bea4661c1c5bb40bf68d34a5
Batool Fatima <batoolfatima2135@gmail.com>
2023-06-05 03:40:25 -0700

    Add comment for cleaner code

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import add from './modules/form-submit.js';
 import BookList from './modules/book-class.js';
 import navbar from './modules/navigation-links.js';
 import date from './modules/datetime.js';
-
-date();
-
-const newBook = new BookList();
-
-document.getElementById('formID').addEventListener('submit', () => add());
-
-newBook.displayBooks();
+// Adding functionality of navbar using navigation-links module
 navbar();
+// Getting date using datetime module
+date();
+// Creating instance of class importing from book-class module
+const newBook = new BookList();
+// Calling eventlistner on form
+document.getElementById('formID').addEventListener('submit', () => add());
+// Calling method to display books using imported class
+newBook.displayBooks();
