e943ed7617f4c9f0da67ae6a80a5eb46a6c6d5c2
Batool Fatima <batoolfatima2135@gmail.com>
2023-06-05 03:14:52 -0700

    Add luxon module for datetime

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import add from './modules/form-submit.js';
 import BookList from './modules/book-class.js';
 import navbar from './modules/navigation-links.js';
+import date from './modules/datetime.js';
+
+date();
 
 const newBook = new BookList();
-add();
+
+document.getElementById('submit-button').addEventListener('click', () => add());
+
 newBook.displayBooks();
 navbar();
--- modules/form-submit.js
+++ modules/form-submit.js
@@ -1,14 +1,11 @@
 import BookList from './book-class.js';
 
 export default function add() {
-  document.querySelector('#submit-button').addEventListener('submit', () => {
-    const newBook = new BookList();
-    const bookTitle = document.querySelector('#book-title').value;
-    const bookAuthor = document.querySelector('#book-author').value;
-    newBook.addBooks(bookTitle, bookAuthor);
-    newBook.displayBooks();
-    document.querySelector('#book-title').value = '';
-    document.querySelector('#book-author').value = '';
-    event.preventDefault();
-  });
+  const newBook = new BookList();
+  const bookTitle = document.querySelector('#book-title').value;
+  const bookAuthor = document.querySelector('#book-author').value;
+  newBook.addBooks(bookTitle, bookAuthor);
+  newBook.displayBooks();
+  document.querySelector('#book-title').value = '';
+  document.querySelector('#book-author').value = '';
 }
